37bde4ccb725c7b5ac1749669f448c675f757296
aaa <p.domkar@seznam.cz>
2014-03-04 20:40:48 +0100

    add toString and equals to Guest

--- src/hotelmanager/Guest.java
+++ src/hotelmanager/Guest.java
@@ -1,38 +1,90 @@
 package hotelmanager;
 
+import java.util.Objects;
+
 /**
  * Entity of Guest
  * @author Petr Domkar & Vojtech Hlavka
  */
 public class Guest {
 
-	private Long id;
-	private String name;
-	private String surname;
-	private String identityCardNumber;
-	private Gender gender;
-        
-        /**
-         * Constructor of entity Guest with no parameters.
-         */
-        public Guest()
-        {
+    private Long id;
+    private String name;
+    private String surname;
+    private String identityCardNumber;
+    private Gender gender;
+
+    /**
+     * Constructor of entity Guest with no parameters.
+     */
+    public Guest()
+    {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getIdentityCardNumber() {
+        return identityCardNumber;
+    }
+
+    public void setIdentityCardNumber(String identityCardNumber) {
+        this.identityCardNumber = identityCardNumber;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Guest{" + "id=" + id + ", name=" + name + ", surname=" + surname + "}";
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + (this.id != null ? this.id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
         }
-        
-        /**
-         * Constructor of entity Guest with 5 parameters.
-         * @param name name of guest
-         * @param surname surname of guest
-         * @param identityCardNumber number of identity cartd of guest
-         * @param gender gender of guest
-         */
-        /*
-        public Guest(String name, String surname, String identityCardNumber, Gender gender)
-        {
-            this.name = name;
-            this.surname = surname;
-            this.identityCardNumber = identityCardNumber;
-            this.gender = gender;
+        if (getClass() != obj.getClass()) {
+            return false;
         }
-        * */
+        final Guest other = (Guest) obj;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }        
 }
