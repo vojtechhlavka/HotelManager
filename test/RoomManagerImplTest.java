5361d018a7314bf12e4ac4bc834f6b9af0c6ba11
aaa <p.domkar@seznam.cz>
2014-03-04 22:08:09 +0100

    RoomManagerImplTest - s error

---
+++ test/RoomManagerImplTest.java
@@ -0,0 +1,20 @@
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Petr
+ */
+public class RoomManagerImplTest {
+    
+    public RoomManagerImplTest() {
+    }
+
+ 
+    @Before
+    public void setUp() {
+    }
+    
+}
