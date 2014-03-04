f72aaaabe9729f868c2c33c6f8c8839afb3409fe
aaa <p.domkar@seznam.cz>
2014-03-04 20:45:20 +0100

    add toString and equals to Room

--- src/hotelmanager/Room.java
+++ src/hotelmanager/Room.java
@@ -1,39 +1,91 @@
 package hotelmanager;
 
+import java.util.Objects;
+
 /**
  * Entity of room
  * @author Petr Domkar & Vojtech Hlavka
  */
 public class Room {
 
-	private Long id;
-	private int number;
-	private int capacity;
-	private int floor;
-	private String note;
-        
-        /**
-         * Constructor of entity room with no parameters.
-         */
-        public Room()
-        {
+    private Long id;
+    private int number;
+    private int capacity;
+    private int floor;
+    private String note;
+
+    /**
+     * Constructor of entity room with no parameters.
+     */
+    public Room()
+    {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" + "id=" + id + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + (this.id != null ? this.id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
         }
-        
-        /**
-         * Constructor of entity room with 5 parameters.
-         * @param id ID of room
-         * @param number number of room
-         * @param capacity capacity of room
-         * @param floor floor of room
-         * @param note note about room
-         */
-        /*
-        public Room(int number, int capacity, int floor, String note)
-        {
-            this.number = number;
-            this.capacity = capacity;
-            this.floor = floor;
-            this.note = note;
+        if (getClass() != obj.getClass()) {
+            return false;
         }
-        */
+        final Room other = (Room) obj;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }        
+    
 }
